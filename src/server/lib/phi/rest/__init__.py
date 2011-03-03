7c105df1911c7e37d26edb41126cf8a68b441ff0
warfares <rodolfo.barriga@gmail.com>
2011-03-03 16:55:01 -0300

    minor adjust for debug log

--- src/server/lib/phi/rest/__init__.py
+++ src/server/lib/phi/rest/__init__.py
@@ -1,7 +1,9 @@
 import logging
 import phi.core.session_helper as session_helper
+import random 
+import sys
 
-LOG_FILENAME = '/log/example.log'
+LOG_FILENAME = '/log/debug.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 session = session_helper.create_session()
@@ -9,12 +11,16 @@ session = session_helper.create_session()
 
 def rest_method(f, *args, **kwargs):
 	def wrapper(*args, **kwargs):
-		logging.debug('rest call ')
+		
+		rid = random.randint(0,10000)
+		logging.debug('BEGIN rest async call:#' + str(rid) + ' ' + f.__name__)
+		
+		#TODO check AUTHENTICATED user
 		
 		r = f(*args, **kwargs)
 		
 		session.close()
 		session.remove()
-		logging.debug('rest call end / session delete')
+		logging.debug('END rest sync call:#' + str(rid) + ' ' + f.__name__)
 		return r
 	return wrapper
