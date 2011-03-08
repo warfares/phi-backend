2043a474e910808495f8cee5e2a9c5af0b2f0e2a
warfares <rodolfo.barriga@gmail.com>
2011-03-08 16:49:09 -0300

    added generic collection filter utility

--- src/server/lib/phi/rest/util.py
+++ src/server/lib/phi/rest/util.py
@@ -14,4 +14,15 @@ def encode_password(passw):
 	hash = hmac.new(SALT,passw, sha256).digest()
 	encoded = base64.b64encode(hash)
 	return encoded
-
+	
+def collection_filter(position, pattern, attribute, collection):
+	if(position == '0' and pattern != '' ):
+		collection = filter(lambda o: getattr(o, attribute).find(pattern) != -1, collection) 
+	if(position == '1'):
+		collection = filter(lambda o: getattr(o, attribute).startswith(pattern), collection)
+	if(position == '2'):
+		collection = filter(lambda o: getattr(o, attribute).endswith(pattern), collection)
+	if(position == '3'):
+		collection = filter(lambda o: getattr(o, attribute).title == pattern, collection)
+	
+	return collection 
