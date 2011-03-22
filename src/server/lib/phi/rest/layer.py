6dc34eebe4c80ab534f4007690e1331401ab56b4
warfares <rodolfo.barriga@gmail.com>
2011-03-22 10:01:22 -0400

    minor adjust

--- src/server/lib/phi/rest/layer.py
+++ src/server/lib/phi/rest/layer.py
@@ -1,19 +1,18 @@
-from bottle import *
+from bottle import get
 import json
-import phi.core.model as model
-import phi.core.repository as repo
 
+import phi.core.repository as repo
 import phi.rest as module
 import phi.rest.vo as vo
 
-@route('layer/all')
+@get('layer/all')
 @module.rest_method
 def all():
 	layers = repo.Layer(session = module.db_session).all()
 	o = map(lambda l: vo.layer(l), layers)
 	return vo.collection(o, len(o))
 
-@route('layer/:id')
+@get('layer/:id')
 @module.rest_method
 def read(id):
 	l = repo.Layer(session = module.db_session).read(id)
